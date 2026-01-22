2be4c35c08a0577329addfaaae8ff17702f23a93
Rachael Cardozo <Rachael.Cardozo@gmail.com>
2026-01-22 14:23:21 -0700

    maint - update script

--- Assets/GMPR2512/Lesson04/DeathZone.cs
+++ Assets/GMPR2512/Lesson04/DeathZone.cs
@@ -5,22 +5,31 @@ namespace Lesson04
     public class DeathZone : MonoBehaviour
     {
         [SerializeField] private int _year = 1000;
+        private float _seconds;
         void Awake()
         {
-            
+            _year += 1026;
+            Debug.Log($"It's the year {_year}!");
         }
 
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         
         void Start()
         {
-            
+            Debug.Log($"It's the year {_year}!");
+            _year++;
         }
 
         // Update is called once per frame
         void Update()
         {
-        
+            
+        }
+
+        void OnTriggerEnter2D(Collider2D collider)
+        {
+            Debug.Log("Collision >:D");
+            Destroy(collider.gameObject);
         }
     }
 }
