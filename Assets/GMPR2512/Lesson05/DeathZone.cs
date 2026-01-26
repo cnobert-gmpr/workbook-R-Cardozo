777d668a8128929a04be6d861524ea6c559b5388
Rachael Cardozo <Rachael.Cardozo@gmail.com>
2026-01-26 14:14:43 -0700

    maint - create spawnpoint

--- Assets/GMPR2512/Lesson05/DeathZone.cs
+++ Assets/GMPR2512/Lesson05/DeathZone.cs
@@ -5,6 +5,7 @@ namespace GMPR2512.Lesson05
 {
     public class DeathZone : MonoBehaviour
     {
+        [SerializeField] private Transform _spawnPoint;
         void OnTriggerEnter2D(Collider2D collider)
         {
             if (collider.gameObject.CompareTag("Ball"))
