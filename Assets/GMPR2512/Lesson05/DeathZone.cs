94ca5e2251ce7da9cb72c66e804aa7fac4d644c6
Rachael Cardozo <Rachael.Cardozo@gmail.com>
2026-01-29 13:21:25 -0700

    maint - add comments for clarification on what code does

--- Assets/GMPR2512/Lesson05/DeathZone.cs
+++ Assets/GMPR2512/Lesson05/DeathZone.cs
@@ -8,6 +8,7 @@ namespace GMPR2512.Lesson05
         [SerializeField] private Transform _spawnPoint;
         void OnTriggerEnter2D(Collider2D collider)
         {
+            // Runs only if game object has the tag 'Ball'
             if (collider.gameObject.CompareTag("Ball"))
             {
                 StartCoroutine(RespawnBall(collider.gameObject));
@@ -18,9 +19,12 @@ namespace GMPR2512.Lesson05
         {
             yield return new WaitForSeconds(2);
             Rigidbody2D BallRB = ball.GetComponent<Rigidbody2D>();
+
+            // Resets velocity to ensure the game won't crash due to velocity of ball moving too fast
             BallRB.linearVelocity = Vector2.zero;
             BallRB.angularVelocity = 0;
 
+            // Respawns ball to preset spawnpoint
             ball.transform.position = _spawnPoint.position;
         }
     }
