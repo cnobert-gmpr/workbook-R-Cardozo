edad60b77597fca7f7f1f274b5938eb3d27c810e
Rachael Cardozo <Rachael.Cardozo@gmail.com>
2026-01-29 13:39:25 -0700

    maint - edit Bumper script

--- Assets/GMPR2512/Lesson05/Bumper.cs
+++ Assets/GMPR2512/Lesson05/Bumper.cs
@@ -4,16 +4,11 @@ namespace GMPR2512.Lesson05
 {
     public class Bumper : MonoBehaviour
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
+        void OnCollisionEnter2D(Collision2D collision)
         {
-        
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-        
+            if(collision.collider.CompareTag("Ball")){
+                Debug.Log($"A game object with the tag {collision.collider.tag} just hit me");
+            }
         }
     }
 }
