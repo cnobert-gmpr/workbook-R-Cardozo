48ffb47aff82339c6820999324f3827748770c47
Rachael Cardozo <Rachael.Cardozo@gmail.com>
2026-03-02 13:06:52 -0700

    maint - copy DeathZone from Assignment02

--- Assets/GMPR2512/Lesson06/DeathZone.cs
+++ Assets/GMPR2512/Lesson06/DeathZone.cs
@@ -6,7 +6,8 @@ namespace GMPR2512.Lesson06
     public class DeathZone : MonoBehaviour
     {
         [SerializeField] private Transform _spawnPoint;
-        [SerializeField] private Bumper _bumper1Script, _bumper2Script;
+        [SerializeField] private Plunger _plunger;
+        [SerializeField] private Flipper _leftFlipper, _rightFlipper;
 
         void OnTriggerEnter2D(Collider2D collider)
         {
@@ -18,13 +19,26 @@ namespace GMPR2512.Lesson06
 
         private IEnumerator RespawnBall(GameObject ball)
         {
-            yield return new WaitForSeconds(1);
+            // Disable plunger and flippers
+            _plunger.DisablePlunger();
+            _leftFlipper.DisableFlipper();
+            _rightFlipper.DisableFlipper();
+
+            // Delay to re-enable plunger and flippers
+            // Set to two seconds to give time to test flippers/plunger before being re-enabled
+            yield return new WaitForSeconds(2);
+
             Rigidbody2D BallRB = ball.GetComponent<Rigidbody2D>();
 
             BallRB.linearVelocity = Vector2.zero;
             BallRB.angularVelocity = 0;
 
             ball.transform.position = _spawnPoint.position;
+
+            // After delay, re-enable flippers and plunger
+            _plunger.EnablePlunger();
+            _leftFlipper.EnableFlipper();
+            _rightFlipper.EnableFlipper();
         }
     }
 }
