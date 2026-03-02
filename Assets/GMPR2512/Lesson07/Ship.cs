a9fce58c8ea33c84de8aa163760c83353d7a79d6
Rachael Cardozo <Rachael.Cardozo@gmail.com>
2026-03-02 13:25:09 -0700

    maint - add simple movement to ship

--- Assets/GMPR2512/Lesson07/Ship.cs
+++ Assets/GMPR2512/Lesson07/Ship.cs
@@ -4,16 +4,9 @@ namespace GMPR2512.Lesson07
 {
     public class Ship : MonoBehaviour
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
-        {
-        
-        }
-
-        // Update is called once per frame
         void Update()
         {
-        
+            transform.Translate(new Vector3(2 * Time.deltaTime, 0, 0));
         }
     }
 }
