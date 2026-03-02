da3acdfa0456a299110f371aba5d89f5c15a35c9
Rachael Cardozo <Rachael.Cardozo@gmail.com>
2026-03-02 13:41:56 -0700

    maint - input actions and create actionMap

--- Assets/GMPR2512/Lesson07/Ship.cs
+++ Assets/GMPR2512/Lesson07/Ship.cs
@@ -1,12 +1,18 @@
+using Codice.Client.Common.GameUI;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace GMPR2512.Lesson07
 {
     public class Ship : MonoBehaviour
     {
+        [SerializeField] private float _movementSpeed = 2f;
+        private InputAction _moveActions;
+
         void Update()
         {
-            transform.Translate(new Vector3(2 * Time.deltaTime, 0, 0));
+
+            transform.Translate(new Vector3(-1 * Time.deltaTime, Time.deltaTime, 0));
         }
     }
 }
