b220e5c1fc809d49d56ea55bbfa0b31721d983e8
Rachael Cardozo <Rachael.Cardozo@gmail.com>
2026-03-02 14:17:23 -0700

    maint - update rotation to limit max rotation limits

--- Assets/GMPR2512/Lesson07/Ship.cs
+++ Assets/GMPR2512/Lesson07/Ship.cs
@@ -1,5 +1,6 @@
 using Codice.Client.BaseCommands.Merge.Restorer.Finder;
 using Codice.Client.Common.GameUI;
+using Codice.CM.Common.Tree;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,7 +8,8 @@ namespace GMPR2512.Lesson07
 {
     public class Ship : MonoBehaviour
     {
-        [SerializeField] private float _movementSpeed = 2, _rotationSpeed = 500;
+        [SerializeField] private float _movementSpeed = 2, _rotationSpeed = 250;
+        [SerializeField] private float _rotationLimit = 45f;
         private InputAction _moveAction, _rotationAction;
 
         void Awake()
@@ -18,13 +20,22 @@ namespace GMPR2512.Lesson07
 
         void Update()
         {
+            // Moves ship left and right
             Vector2 moveDirection = new Vector2(_moveAction.ReadValue<Vector2>().x, 0);
             Vector2 translation = moveDirection.normalized * _movementSpeed * Time.deltaTime;
             transform.Translate(translation, Space.Self);
 
+            // Takes up/down input to rotate ship
             float rotationInput = _rotationAction.ReadValue<Vector2>().y;
             float rotationAmount = rotationInput * _rotationSpeed * Time.deltaTime;
             transform.Rotate(Vector3.forward, -rotationAmount);
+
+            // Prevents rotation from going past the specified rotation limits
+            Vector3 euler = transform.eulerAngles;
+
+            if(euler.z > 180f) euler.z -= 360f;
+            euler.z = Mathf.Clamp(euler.z, -_rotationLimit, _rotationLimit);
+            transform.eulerAngles = euler;
         }
     }
 }
