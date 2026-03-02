2362be4aa50b98e13388df37773cdd71aebbc80e
Rachael Cardozo <Rachael.Cardozo@gmail.com>
2026-03-02 13:05:56 -0700

    maint - copy flipper from assignment02

--- Assets/GMPR2512/Lesson06/Flipper.cs
+++ Assets/GMPR2512/Lesson06/Flipper.cs
@@ -4,23 +4,34 @@ namespace GMPR2512.Lesson06
 {
     public class Flipper : MonoBehaviour
     {
-        private HingeJoint2D _joint2D;
+            private HingeJoint2D _joint2D;
+            private bool _enabled = true;
 
-        void Awake()
-        {
-            _joint2D = GetComponent<HingeJoint2D>();
-        }
-        void Update()
-        {
-            if (_joint2D.CompareTag("Left"))
+            void Awake()
             {
-                _joint2D.useMotor = Input.GetKey(KeyCode.LeftShift);
+                _joint2D = GetComponent<HingeJoint2D>();
             }
 
-            if (_joint2D.CompareTag("Right"))
+            public void EnableFlipper() => _enabled = true;
+            public void DisableFlipper() => _enabled = false;
+
+            void Update()
             {
-                _joint2D.useMotor = Input.GetKey(KeyCode.RightShift);
+                if (!_enabled)
+                {
+                    _joint2D.useMotor = false;
+                    return;
+                }
+
+                if (_joint2D.CompareTag("LeftFlipper"))
+                {
+                    _joint2D.useMotor = Input.GetKey(KeyCode.LeftShift);
+                }
+
+                if (_joint2D.CompareTag("RightFlipper"))
+                {
+                    _joint2D.useMotor = Input.GetKey(KeyCode.RightShift);
+                }
             }
-        }
     }
 }
