1d8e5cb6abe34f86b5ea1698e137a6cf8a10923a
Rachael Cardozo <Rachael.Cardozo@gmail.com>
2026-03-05 13:56:07 -0700

    maint - update Projectile class

--- Assets/GMPR2512/Lesson07/Projectile.cs
+++ Assets/GMPR2512/Lesson07/Projectile.cs
@@ -7,6 +7,10 @@ namespace GMPR2512.Lesson07
         private float _speed = 10, _spinVelocity;
         private Vector2 _direction = Vector2.up;
 
+        internal Vector2 Direction { get => _direction; set => _direction = value; }
+        
+        internal float Speed { get => _speed; set => _speed = value; }
+
         void Update()
         {
             transform.Translate(_direction.normalized * _speed * Time.deltaTime, Space.World);
