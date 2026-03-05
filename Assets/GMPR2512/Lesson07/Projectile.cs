0972c410de7c19122d2dadec8709ee9c6efbcbc6
Rachael Cardozo <Rachael.Cardozo@gmail.com>
2026-03-05 13:37:08 -0700

    maint - add variables to Projectile class

---
+++ Assets/GMPR2512/Lesson07/Projectile.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace GMPR2512.Lesson07
+{
+    public class Projectile : MonoBehaviour
+    {
+        private float _speed, _spinVelocity;
+        private Vector2 _direction;
+
+        void Update()
+        {
+            
+        }
+    }
+}
