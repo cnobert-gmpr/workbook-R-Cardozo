cfeb19c0ad5127702883eaa8e62ce9c5c60d1e5e
Rachael Cardozo <Rachael.Cardozo@gmail.com>
2026-03-19 13:40:44 -0600

    maint - update ButtonHandler

--- Assets/GMPR2512/Lesson08/Scene00_Button_Handler.cs
+++ Assets/GMPR2512/Lesson08/Scene00_Button_Handler.cs
@@ -6,6 +6,25 @@ namespace GMPR2512.Lesson08
 {
     public class Scene00_Button_Handler : MonoBehaviour
     {
-        
+        private Scene00_Button_Handler _changeToScene01Button;
+
+        private void OnEnable()
+        {
+            VisualElement root = GetComponent<>().rootVisualElement;
+            _changeToScene01Button = root.Q<Button>("ChangeToScene01Button");
+
+            if(_changeToScene01Button != null)
+            {
+                _changeToScene01Button.clicked += ChangeToScene01;
+            }
+        }
+
+        private void OnDisable()
+        {
+            if(_changeToScene01Button != null)
+            {
+                _changeToScene01Button.clicked -= ChangeToScene01;
+            }
+        }
     }
 }
