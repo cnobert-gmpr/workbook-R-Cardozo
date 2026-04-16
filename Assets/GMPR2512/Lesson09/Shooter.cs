6138f047789d0ad98f891071b0133e527319a8c8
Rachael Cardozo <Rachael.Cardozo@gmail.com>
2026-04-16 13:40:36 -0600

    maint - update Shooter class

--- Assets/GMPR2512/Lesson09/Shooter.cs
+++ Assets/GMPR2512/Lesson09/Shooter.cs
@@ -9,12 +9,22 @@ namespace GMPR2512.Lesson09
             float rotationInput = 0;
 
             if(Input.GetKey(KeyCode.Comma))
-                rotationInput = 1;
+                rotationInput = 125;
             else if(Input.GetKey(KeyCode.Period))
-                rotationInput = -1;
+                rotationInput = -125;
             
             rotationInput *= Time.deltaTime;
             transform.parent.Rotate(new Vector3(0, 0, rotationInput));
+
+            int layerMask = LayerMask.GetMask("Ground", "Enemy");
         }
+
+        /**
+        void OnDrawGizmo()
+        {
+            Gizmos.color = Color.yellowNice;
+            Gizmos.DrawRay(transform.position, transform.right * 10);
+        }
+        **/
     }
 }
